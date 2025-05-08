3f84beecc0f735b8122e0f746d7783795e0fcf99
Leonardo Silveira <sombriks@gmail.com>
2025-05-08 10:47:49 -0300

    prototyping

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fs from "fs"
 import path from "path"
+import { title } from "process"
 
 // 1- find the markdown file (default is ./README.md)
 const file = "README.md"
@@ -8,9 +9,9 @@ const content = fs.readFileSync(path.join(".", file), { encoding: "utf-8" }).spl
 // 2 - parse file (titles and block scripts)
 let blockOpen = false
 const filtered = content.map(line => {
-  if (line.trim().startsWith("#"))
+  if (line.trim().startsWith("#")) {
     return line
-  else if (line.trim().startsWith("```bash")) {
+  } else if (line.trim().startsWith("```bash")) {
     blockOpen = true
     return line
   } else if (line.trim().startsWith("```")) {
@@ -22,9 +23,23 @@ const filtered = content.map(line => {
 }).filter(line => "" != line)
 const parsed = filtered.reduce((acc, line) => {
   // set title 
-  // add script
+  if (!acc.scriptOpen && line.trim().startsWith("#")) {
+    acc.currentTitle = line
+  } else if (line.trim().startsWith("```bash")) {
+    acc.scriptOpen = true
+  } else if (line.trim().startsWith("```")) {
+    if (!acc.parsedScripts[acc.currentTitle]) {
+      acc.parsedScripts[acc.currentTitle] = []
+    }
+    acc.parsedScripts[acc.currentTitle].push(acc.currentScripts)
+    acc.currentScripts = []
+    acc.scriptOpen = false
+  } else {
+    // add script
+    acc.currentScripts.push(line)
+  }
   return acc
-}, {})
+}, { scriptOpen: false, currentTitle: "", currentScripts: [], parsedScripts: {} })
 console.log(parsed)
 // 
 // 3- list all script blocks marked as bash

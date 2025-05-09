0b426a035dc46778512d25841053f318bde837dd
Leonardo Silveira <sombriks@gmail.com>
2025-05-09 17:19:29 -0300

    fixes #5

--- test/executor.spec.js
+++ test/executor.spec.js
@@ -1,8 +1,15 @@
 import test from "ava"
 import {runBashScript} from "../lib/executor.js"
 
-test('should execute', t => {
+test("should execute", t => {
   t.notThrows(() => runBashScript({script: 0}, {
     allScripts:[["echo hello world!"]]
   }, "."))
+})
+
+test("should read stdin", t => {
+  runBashScript({script: 0}, {
+    allScripts:[["read i", "echo $i!"]]
+  }, ".")
+  t.pass()
 })
---
+++ test/fixtures/sample.md
@@ -0,0 +1,13 @@
+# sample
+
+some script blocks
+
+```bash
+echo "hello!"
+```
+
+```bash
+echo "what is your name?"
+read i
+echo "hello $i!"
+```

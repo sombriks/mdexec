5424206b9a833b79eff8438cdd9492add8d1f797
Leonardo Silveira <sombriks@gmail.com>
2025-05-09 13:23:26 -0300

    works on #1 and #2

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -1,5 +1,21 @@
-import test from 'ava'
+import test from "ava"
+import {parseContent} from "../lib/parser.js"
 
-test('parseContent', t => {
-  t.pass()
+test('should parse content', t => {
+  const result = parseContent([
+    "#Example",
+    "",
+    "```bash",
+    "echo 'sample'",
+    "```",
+  ])
+  t.deepEqual(result.parsedScripts, {'#Example': [["echo 'sample'"]]})
+})
+
+test('should fail - invalid content', t => {
+  t.throws(() => parseContent([
+    "#Example",
+    "",
+    "```bash",
+  ]))
 })

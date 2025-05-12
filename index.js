1db7131f9dc726796dd0ceb778e44bf2ac7b41a8
Leonardo Silveira <sombriks@gmail.com>
2025-05-12 10:35:29 -0300

    add help message

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-import {listScripts} from "./lib/listing.js"
-import {getParameters} from "./lib/parameters.js"
-import {parseContent} from "./lib/parser.js"
-import {runBashScript} from "./lib/executor.js"
+import { listScripts } from "./lib/listing.js"
+import { getParameters } from "./lib/parameters.js"
+import { parseContent } from "./lib/parser.js"
+import { runBashScript } from "./lib/executor.js"
 
-const {options, cwd, file, content} = getParameters()
+const { options, cwd, file, content } = getParameters()
 const parsed = parseContent(content)
-if (options.list) listScripts(file, parsed)
+if (options.help) {
+  console.log(`
+  Usage: mdexec [options] <number>
+
+  Options:
+    -h, --help       Show this help message
+    -d, --directory  Set the working directory for the script
+    -f, --file       Specify the markdown file to execute
+    -l, --list       List all scripts in the markdown file
+    [-s, --script]   Run a specific script by number
+  `)
+  process.exit(0)
+} else if (options.list) listScripts(file, parsed)
 if (options.script !== undefined) runBashScript(options, parsed, cwd)
--- test/listing.spec.js
+++ test/listing.spec.js
@@ -1,10 +1,13 @@
 import test from "ava"
-import {listScripts} from "../lib/listing.js"
+import { listScripts } from "../lib/listing.js"
 
 test("should print", t => {
-  t.notThrows(() => listScripts("README.md", {
-    parsedScripts: {
-      "#Title": [["echo 'testing...'"]]
-    }
-  }))
+  t.notThrows(() => {
+    listScripts("README.md", {
+      parsedScripts: {
+        "#Title": [["echo 'testing...'"]]
+      }
+    })
+    t.pass()
+  })
 })
